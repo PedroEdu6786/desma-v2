d76fee8ef39e98f2a5acdd60e0a3d1436b3109df
César González <chesarglez429@gmail.com>
2022-12-02 12:39:46 -0600

    :construction: Crete a main layout and use it in the dashboard page

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { Layout } from "../components/Layout";
+
+const Dashboard: NextPage = (props) => {
+    return (
+        <Layout>
+
+        </Layout>
+    )
+}
+
+export default Dashboard
