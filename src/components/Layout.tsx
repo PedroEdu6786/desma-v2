5ca56dc7b7ca027f4743d41831a68d01758771ba
César González <chesarglez429@gmail.com>
2022-12-02 12:44:00 -0600

    :art: Run the prettier formatter in the layout component

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,30 +1,24 @@
-import { Box, Divider, Flex, Link, Spacer, VStack, Heading } from "@chakra-ui/react"
-import styles from './Layout.module.scss'
+import { Box, Divider, Flex, Link, Spacer, VStack, Heading } from '@chakra-ui/react';
+import styles from './Layout.module.scss';
 
 const Layout: React.FC = ({ children }) => {
   return (
-    <VStack m={8} align='stretch' justify='center'>
+    <VStack m={8} align="stretch" justify="center">
       <Flex>
-        <Link href='/dashboard'>
-          <Heading
-            as='h2'
-            fontSize="xl"
-            className={styles.yellow_to_red_gradient}
-          >
+        <Link href="/dashboard">
+          <Heading as="h2" fontSize="xl" className={styles.yellow_to_red_gradient}>
             Desma
           </Heading>
         </Link>
         <Spacer />
-        <Box>
-          Avatar
-        </Box>
+        <Box>Avatar</Box>
       </Flex>
 
       <Divider />
-      
+
       {children}
     </VStack>
-  )
-}
+  );
+};
 
-export { Layout }
+export { Layout };
