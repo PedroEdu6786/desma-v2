69cb1f06d9876a270b1797a820d6e62c5f5f6fc7
Pedro Eduardo Cruz de la Fuente <unholymac@Pedros-MacBook-Air.local>
2022-12-03 11:54:19 -0600

    feat: add useToast hook

---
+++ src/hooks/useToast.hook.tsx
@@ -0,0 +1,40 @@
+import { useToast as useChakraToast, UseToastOptions } from '@chakra-ui/react';
+
+const toastBaseConfig = (message: string): UseToastOptions => ({
+  duration: 6000,
+  isClosable: true,
+  position: 'top-right',
+  description: message,
+});
+
+const useToast = () => {
+  const toast = useChakraToast();
+
+  const callAlertToast = (message: string) => {
+    return toast({
+      title: 'Warning',
+      status: 'warning',
+      ...toastBaseConfig(message),
+    });
+  };
+
+  const callSuccessToast = (message: string) => {
+    return toast({
+      title: 'Success request',
+      status: 'success',
+      ...toastBaseConfig(message),
+    });
+  };
+
+  const callFailToast = (message: string) => {
+    return toast({
+      title: 'Failed request',
+      status: 'error',
+      ...toastBaseConfig(message),
+    });
+  };
+
+  return { callFailToast, callSuccessToast, callAlertToast };
+};
+
+export default useToast;
