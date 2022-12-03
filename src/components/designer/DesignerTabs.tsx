e1eda5c0432813e5d6ead8050fdb5174dc6154fa
César González <chesarglez429@gmail.com>
2022-12-03 01:42:27 -0600

    :art: Remove explicit return

--- src/components/designer/DesignerTabs.tsx
+++ src/components/designer/DesignerTabs.tsx
@@ -1,31 +1,30 @@
 import { Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react';
 
-const DesignerTabs = () => {
-  return (
-    <Tabs>
-      <TabList>
-        <Tab>Colors</Tab>
-        <Tab>Fonts</Tab>
-        <Tab>Spacing</Tab>
-        <Tab>Preview</Tab>
-      </TabList>
+// This component should not have state, it only groups the sections to avoid code repetition
+const DesignerTabs = () => (
+  <Tabs>
+    <TabList>
+      <Tab>Colors</Tab>
+      <Tab>Fonts</Tab>
+      <Tab>Spacing</Tab>
+      <Tab>Preview</Tab>
+    </TabList>
 
-      <TabPanels>
-        <TabPanel>
-          <p>one!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>two!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>three!</p>
-        </TabPanel>
-        <TabPanel>
-          <p>four!</p>
-        </TabPanel>
-      </TabPanels>
-    </Tabs>
-  );
-};
+    <TabPanels>
+      <TabPanel>
+        <p>one!</p>
+      </TabPanel>
+      <TabPanel>
+        <p>two!</p>
+      </TabPanel>
+      <TabPanel>
+        <p>three!</p>
+      </TabPanel>
+      <TabPanel>
+        <p>four!</p>
+      </TabPanel>
+    </TabPanels>
+  </Tabs>
+);
 
 export { DesignerTabs };
