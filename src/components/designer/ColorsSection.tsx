8da64581dc23a2771276d1816b4e84a9b9f0c1d9
César González <chesarglez429@gmail.com>
2022-12-05 03:39:50 -0600

    :sparkles: Implement the colors section

--- src/components/designer/ColorsSection.tsx
+++ src/components/designer/ColorsSection.tsx
@@ -1,7 +1,29 @@
-export type ColorsSectionProps = {};
+import { Flex, Heading, VStack } from '@chakra-ui/react';
+import { EditColors } from './EditColors';
 
-const ColorsSection: React.FC<ColorsSectionProps> = () => {
-  return <p>one!</p>;
+export type ColorGroup = {
+  label: string;
+  colors: string[];
+};
+
+export type ColorsSectionProps = {
+  colorGroups: ColorGroup[];
+  setColors: (groupIndex: number) => (colors: string[]) => void;
+};
+
+const ColorsSection: React.FC<ColorsSectionProps> = ({ colorGroups, setColors }) => {
+  return (
+    <Flex alignContent="start" gap={8} wrap="wrap">
+      {colorGroups.map(({ label, colors }, groupIndex) => (
+        <VStack spacing={4} key={label} align="initial">
+          <Heading as="h3" size="md">
+            {label}
+          </Heading>
+          <EditColors colors={colors} setColors={setColors(groupIndex)} />
+        </VStack>
+      ))}
+    </Flex>
+  );
 };
 
 export { ColorsSection };
